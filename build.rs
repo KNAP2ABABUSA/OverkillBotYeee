2cfdd018a17a686bfba852f6387eeb5380912368
KNAP2ABABUSA <kostya.nikolayev.11@internet.ru>
2026-03-09 21:45:17 +0300

    Update build.rs
    I first ran the build from GitHub and it didnt work, but I did a great job fixing everything

--- build.rs
+++ build.rs
@@ -1,18 +1,19 @@
 fn main() {
     println!("cargo:rerun-if-changed=cpp/turbo_engine.cpp");
     println!("cargo:rerun-if-changed=cpp/include/turbo_engine.h");
+    println!("cargo:warning=Build script started");
+    println!("cargo:rustc-link-search=native=/usr/lib/x86_64-linux-gnu");
+    println!("cargo:rustc-link-search=native=/lib/x86_64-linux-gnu");
+    println!("cargo:rustc-link-arg=-lcurl");
+    println!("cargo:rustc-link-arg=-lssl");
+    println!("cargo:rustc-link-arg=-lcrypto");
+    println!("cargo:warning=pkg-config libcurl found");
     cc::Build::new()
         .cpp(true)
         .file("cpp/turbo_engine.cpp")
         .include("cpp/include")
+        .include("/usr/include/x86_64-linux-gnu")
         .flag("-std=c++11")
         .flag("-fPIC")
         .compile("turbo_engine");
-    println!("cargo:rustc-link-search=native=/usr/lib/x86_64-linux-gnu");
-    println!("cargo:rustc-link-search=native=/lib/x86_64-linux-gnu");
-    println!("cargo:rustc-link-lib=dylib=curl");
-    println!("cargo:rustc-link-lib=dylib=ssl");
-    println!("cargo:rustc-link-lib=dylib=crypto");
-    println!("cargo:rustc-link-lib=dylib=stdc++");
-}
-//Черт(тут должно быть другое слово) что я написал вообще
+    println!("cargo:warning=Compilation finished");}
